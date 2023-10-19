e485901d8f63a846cbe53d9843eefe0686eae32c
Kit <kitavendano@gmail.com>
2023-10-19 22:37:29 +0800

    update index page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./*.{html,js,css}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: {
+        white: "hsl(0, 0%, 100%)",
+        lightgray: "hsl(212, 45%, 89%)",
+      },
+      textColor: {
+        grayish: "hsl(220, 15%, 55%)",
+        darkblue: "hsl(218, 44%, 22%)",
+      },
+    },
   },
   plugins: [],
-}
-
+};
